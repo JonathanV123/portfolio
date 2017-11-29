1341f00306c778bbac495666e63ac5a7f15ba71e
Jonathan <jvoxland@gmail.com>
2017-11-29 15:11:40 -0800

    added correct links to blogs and changed text layout of tutorial component

--- src/components/Tutorials_Blog.js
+++ src/components/Tutorials_Blog.js
@@ -7,7 +7,7 @@ import twitter_icon from '../assets/images/twitter_icon.png';
 export default class Tutorials_Blog extends React.Component {
     render() {
         const opts = {
-            playerVars: { // https://developers.google.com/youtube/player_parameters
+            playerVars: {
                 height: '400',
                 width: '575',
                 autoplay: 0
@@ -20,29 +20,36 @@ export default class Tutorials_Blog extends React.Component {
                 </h2>
 
                 <YouTube
-                    videoId="2g811Eo7K8U"
+                    videoId="SLa4U3w5DV8"
                     opts={opts}
                     onReady={this._onReady}
                 />
-                <p>
+                <a id="youtubeLink" href="https://www.youtube.com/channel/UC8ekmpR_7oSwbXVfvMQvVfg/featured" target="_blank">
+                    <h3>Visit my channel</h3>
+                </a>
+                <p className="centerText blog_text">
                     My personal Web Development Youtube Channel and Blog focusing on teaching others about HTML, CSS,
                     Javascript, and other aspects of web development.
                 </p>
                 <hr/>
-                <h2>
+                <h2 className="centerText">
                     Web development blog on Medium
                 </h2>
+                <a href="https://medium.com/@jonathanvox01" target="_blank">
                     <img className="icon" src={medium_icon} alt="Medium Icon"/>
-                <p className="centerText">
+                </a>
+                <p className="centerText blog_text">
                     My coding blog aimed at teaching people different aspects of Javascript with relevant and easy to
                     understand examples.
                 </p>
                 <hr/>
-                <h2>
+                <h2 className="centerText">
                     All things code on my Twitter
                 </h2>
+                <a href="https://twitter.com/Mecha_Jiv" target="_blank">
                     <img className="icon" src={twitter_icon} alt="Twitter Icon"/>
-                <p className="centerText">
+                </a>
+                <p className="centerText blog_text">
                     Check out my twitter for snippets of interesting code, and announcements of upcoming topics for my
                     Youtube channel and blog.
                 </p>
